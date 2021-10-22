7a632065fb7e133610ddf600ae5a56bd6c4367fd
jonbaird <jbaird29@gmail.com>
2021-10-21 20:38:38 -0400

    add saving of profile information

--- screens/EditProfileScreen.tsx
+++ screens/EditProfileScreen.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useEffect, useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -14,19 +15,45 @@ import {
 import IncludeInfoRow from "../components/IncludeInfoRow";
 import { IncludeInfoRowProps, schema } from "../schema";
 
-export default function EditProfileScreen({ navigation }) {
+export default function EditProfileScreen({ navigation, route }) {
+  const { profileName } = route.params;
+
   const includeInfoFields: IncludeInfoRowProps[] = schema.map((field) => {
     const [isEnabled, setEnabled] = useState(false);
     return { ...field, isEnabled: isEnabled, setEnabled: setEnabled };
   });
 
-  const handleSave = () => {
-    console.log("saved"); // TODO - save this info to local storage
-    navigation.navigate("MyProfile", {});
+  const handleSave = async () => {
+    try {
+      includeInfoFields.forEach(async ({ key, isEnabled }) => {
+        await AsyncStorage.setItem(`@${profileName}-${key}-toInclude`, isEnabled.toString());
+      });
+      navigation.navigate("MyProfile", { profileName: profileName });
+    } catch (e) {
+      console.log(e);
+    }
   };
+
+  const loadData = async () => {
+    try {
+      // TODO: consider refactoring to multiGet(): https://react-native-async-storage.github.io/async-storage/docs/api#multiget
+      includeInfoFields.forEach(async ({ key, setEnabled }) => {
+        const isEnabled = await AsyncStorage.getItem(`@${profileName}-${key}-toInclude`);
+        console.log(key, isEnabled);
+        setEnabled(isEnabled === "true" ? true : false); // must convert string to boolean
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
+      <Text>{profileName} Profile</Text>
       {includeInfoFields.map(({ key, isEnabled, setEnabled, label }) => (
         <IncludeInfoRow key={key} isEnabled={isEnabled} setEnabled={setEnabled} label={label} />
       ))}
--- screens/MyProfileScreen.tsx
+++ screens/MyProfileScreen.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import { StyleSheet, Text, View, Button, Image } from "react-native";
 import { NavigationProp } from "@react-navigation/native";
 
-export default function MyProfileScreen({ navigation }) {
-  const handleEditPress = () => navigation.navigate("EditProfile", {});
+export default function MyProfileScreen({ navigation, route }) {
+  const { profileName } = route.params; // TODO - make this screen dynamic for different profiles
+
+  const handleEditPress = () => navigation.navigate("EditProfile", { profileName: "Professional" });
   const handleSharePress = () => {};
 
   return (
     <View style={styles.container}>
+      <Text>Professional Profile</Text>
       <Image resizeMode="contain" source={require("../assets/fake-qr.jpg")} style={{ width: "75%" }} />
       <Button title="Edit this Profie" onPress={handleEditPress} />
-      <Button title="Share as Link (Not yet Functional)" onPress={handleSharePress} />
     </View>
   );
 }
