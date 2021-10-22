3ec2ef6232b968981b72920883c80754baeb576f
jonbaird <jbaird29@gmail.com>
2021-10-22 17:08:49 -0400

    add barcode reader (needs testing)

--- screens/ScanQRCodeScreen.tsx
+++ screens/ScanQRCodeScreen.tsx
@@ -1,11 +1,39 @@
-import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
-import { NavigationProp } from "@react-navigation/native";
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
+// Copied from: https://docs.expo.dev/versions/v43.0.0/sdk/bar-code-scanner/
+// Date: 10/22/2021
 export default function ScanQRCodeScreen({ navigation }) {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>TO BE UPDATED! STAY TUNED!</Text>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />}
     </View>
   );
 }
