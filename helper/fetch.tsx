3045902ec82ab70ee61b06cc0669d70495661536
jonbaird <jbaird29@gmail.com>
2021-11-04 12:38:29 -0400

    update fetch to use teammate's microservice

--- helper/fetch.tsx
+++ helper/fetch.tsx
@@ -1,16 +1,22 @@
 import { InfoSchema, SomeInfoSchema } from "../schema";
 
 export const fetchQR = async (profileInfo: SomeInfoSchema) => {
-  const response = await fetch("http://localhost:3000", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(profileInfo),
-  });
-  if (response.ok) {
-    return await response.text();
-  } else {
+  console.log(`Fetching: ${JSON.stringify(profileInfo)}`);
+  try {
+    const response = await fetch("https://qr-generator290.herokuapp.com/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(profileInfo),
+    });
+    if (response.ok) {
+      return await response.text();
+    } else {
+      return false;
+    }
+  } catch (err) {
+    console.log(err);
     return false;
   }
 };
